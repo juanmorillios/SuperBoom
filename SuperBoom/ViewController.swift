1b214b4e7aff73b75c80b021c5745c360e8fa836
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-09-11 17:35:18 +0200

    SuperBoom
    SuperBoom

--- SuperBoom/ViewController.swift
+++ SuperBoom/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIAlertViewDelegate {
 
+    @IBOutlet weak var imgBlack: UIImageView!
+    @IBOutlet weak var imgRed: UIImageView!
+    @IBOutlet weak var imgBoomBg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressBtnBoomBlack(sender: AnyObject) {
+        
+        imgBlack.hidden = true
+        imgBoomBg.hidden = false
+        
+    let alert = UIAlertView (title: "Alert!!!!", message: "A desactivado la bomba Black", delegate: nil, cancelButtonTitle: "Aceptar")
+        
+        alert.show()
+        
+        
+    }
 
+    @IBAction func pressBtnBoomRed(sender: AnyObject) {
+        
+        imgRed.hidden = true
+        
+        imgBoomBg.hidden = false
+        
+        let alert = UIAlertView (title: "Alert!!!!", message: "A desactivado la bomba Red", delegate: nil, cancelButtonTitle: "Aceptar")
+        
+        alert.show()
+
+        
+    }
 }
 
