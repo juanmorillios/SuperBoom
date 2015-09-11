551af8fae40bee4c6f0e6a277985315885df3b44
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-09-11 16:56:18 +0200

    Initial Commit

---
+++ SuperBoom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperBoom
+//
+//  Created by Juan Morillo on 11/9/15.
+//  Copyright (c) 2015 JuanMorillios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
