680b2b4a3213de188f417a4b09a68af5a8bab6f3
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-09-14 00:04:34 +0200

    Update game of bomb
    Add score + punts to show of all players.

--- SuperBoom/ViewController.swift
+++ SuperBoom/ViewController.swift
@@ -13,17 +13,28 @@ class ViewController: UIViewController, UIAlertViewDelegate {
     @IBOutlet weak var imgBlack: UIImageView!
     @IBOutlet weak var imgRed: UIImageView!
     @IBOutlet weak var imgBoomBg: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var jugador: UILabel!
+    @IBOutlet weak var bombas: UILabel!
+    
+    var colorOfBombasArray:[String] = ["boom","boom1", "boomb2","boom3","boom4","boom5","boom6","boom7"]
+    var jugadorScore = 0
+    var bombasScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
+    
     }
 
-    @IBAction func pressBtnBoomBlack(sender: AnyObject) {
+   /* @IBAction func pressBtnBoomBlack(sender: AnyObject) {
         
         // Proceso para ocultar las imagenes
         
@@ -56,6 +67,49 @@ class ViewController: UIViewController, UIAlertViewDelegate {
         alert.show()
 
         
+    } */
+    
+    
+    @IBAction func pressPlayGameBombaBtn(sender: AnyObject) {
+        
+      
+        
+        let firstRamdonBombaNumber:Int = Int(arc4random_uniform(4))
+        
+        let firstContenedorBombaString:String = self.colorOfBombasArray[firstRamdonBombaNumber]
+        
+        self.imgBlack.image = UIImage(named: firstContenedorBombaString)
+        
+    
+        let secondRamdomBombaNumber:Int = Int(arc4random_uniform(4))
+        
+        let secondContenedorBombasString:String = self.colorOfBombasArray[secondRamdomBombaNumber]
+        
+        self.imgRed.image = UIImage(named: secondContenedorBombasString)
+        
+        if firstRamdonBombaNumber > secondRamdomBombaNumber{
+        
+        jugadorScore += 1
+        self.jugador.text = String(jugadorScore)
+            
+        
+        } else if firstRamdonBombaNumber == secondRamdomBombaNumber {
+        
+        
+            
+        
+        } else {
+        
+        
+            bombasScore += 1
+            self.bombas.text = String(bombasScore)
+        
+        }
+        
+        
+        
     }
+   
+    
 }
 
